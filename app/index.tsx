5cf84a866aa47cd65f493eb7f244e5e6c29d9e7f
abanoub-abdelmessih <abanoubabdelmessih110@gmail.com>
2026-01-15 18:34:23 +0200

    Change scrollView to FlatList , Add Card Color based on pokemon type

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,45 @@
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+
+interface PokemonListItem {
+  name: string;
+  url: string;
+}
 
 interface Pokemon {
   name: string;
   front_image: string;
   back_image: string;
+  types: PokemonType[];
 }
+
+interface PokemonType {
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+const colorsByType: Record<string, string> = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
 
 export default function Index() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -20,13 +54,14 @@ export default function Index() {
     const data = await response.json();
 
     const detailedPokemons = await Promise.all(
-      data.results.map(async (pokemon: any) => {
+      data.results.map(async (pokemon: PokemonListItem) => {
         const res = await fetch(pokemon.url);
         const details = await res.json();
         return {
           name: pokemon.name,
           front_image: details.sprites.front_default,
           back_image: details.sprites.back_default,
+          types: details.types,
         };
       })
     );
@@ -34,26 +69,61 @@ export default function Index() {
     setPokemons(detailedPokemons);
   }
 
-  return (
-    <ScrollView>
-      {pokemons.map((pokemon) => (
-        <View key={pokemon.name}>
-          {/* Pokemon Name */}
-          <Text>{pokemon.name}</Text>
+  type PokemonTypeName = keyof typeof colorsByType;
 
-          {/* Images View */}
-          <View style={{ flexDirection: "row" }}>
+  function getPokemonColor(types: PokemonType[]): string {
+    const primaryType = types[0]?.type.name as PokemonTypeName | undefined;
+    return primaryType ? colorsByType[primaryType] + 50 : "#AAA";
+  }
+
+  return (
+    <FlatList
+      data={pokemons}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{ gap: 16, padding: 16 }}
+      renderItem={({ item }) => (
+        <View
+          style={{
+            backgroundColor: getPokemonColor(item.types),
+            padding: 20,
+            borderRadius: 20,
+          }}
+        >
+          {/* Name */}
+          <Text style={styles.name}>{item.name}</Text>
+
+          {/* Types */}
+          <Text style={styles.type}>
+            {item.types.map((t) => t.type.name).join(", ")}
+          </Text>
+
+          {/* Images */}
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
             <Image
-              source={{ uri: pokemon.front_image }}
-              style={{ width: 150, height: 150 }}
+              source={{ uri: item.front_image }}
+              style={{ width: 120, height: 120 }}
             />
             <Image
-              source={{ uri: pokemon.back_image }}
-              style={{ width: 150, height: 150 }}
+              source={{ uri: item.back_image }}
+              style={{ width: 120, height: 120 }}
             />
           </View>
         </View>
-      ))}
-    </ScrollView>
+      )}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  type: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "gray",
+    textAlign: "center",
+  },
+});
